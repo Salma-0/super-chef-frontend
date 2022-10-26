ca37908c751f25d3519f5d1491615a9e7cd56d8b
salmehdhar <salmehdhar@hsoub.com>
2022-10-26 15:31:39 +0300

    add share buttons & og tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,38 +5,56 @@ import Header from './Header'
 import Footer from './Footer'
 import 'react-toastify/dist/ReactToastify.css';
 
+interface RecipeTags {
+    title: string,
+    description: string,
+    image: string,
+    url: string
+}
 
 
 interface Props {
     children: ReactChild[] | ReactChild | Element,
-    keywords: string, 
+    keywords: string,
     description: string,
-    title: string
+    title: string,
+    recipeData: RecipeTags | null
 }
 
 
 
-export default function  Layout({children,  title='Super Chef', keywords='recipes, cusine, food, dessert, cooking, chef', description='deliciouse recipes'}: Props){
+export default function Layout({ children, title = 'Super Chef', keywords = 'recipes, cusine, food, dessert, cooking, chef', description = 'deliciouse recipes', recipeData = null }: Props) {
     return (
-       <div className={styles.container}>
-           <Head>
-               <title>{title}</title>
-               <meta name="description" content={description} />
-               <meta name='keywords' content={keywords}/>
-               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-           </Head>
-           <Header/>
-           <main className={styles.main}>
-              {children}
-           </main>
-           <Footer/>
-       </div>
+        <div className={styles.container}>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name='keywords' content={keywords} />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                {recipeData && (
+                    <>
+                        <meta name="og:title" content={recipeData.title} />
+                        <meta name="og:description" content={recipeData.description} />
+                        <meta name="og:url" content={recipeData.url} />
+                        <meta name="og:image" content={recipeData.image} />
+                    </>
+                )}
+            </Head>
+            <Header />
+            <main className={styles.main}>
+                {children}
+
+            </main>
+            <Footer />
+        </div>
     )
 }
 
 
 Layout.defaultProps = {
     title: 'SuperChef',
-    keywords:'recipes, cusine, food, dessert, cooking, chef',
-    description:'deliciouse recipes'
+    keywords: 'recipes, cusine, food, dessert, cooking, chef',
+    description: 'deliciouse recipes',
+    recipeData: null
 }
--- pages/recipes/[slug].tsx
+++ pages/recipes/[slug].tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import Layout from '@components/Layout'
 import styles from '@styles/Recipe.module.css'
 import Image from 'next/image'
@@ -10,6 +10,8 @@ import { GetServerSidePropsContext } from 'next'
 import Error from 'next/error'
 import {API_URL} from 'config/index'
 import axios from 'axios'
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, WhatsappShareButton, WhatsappIcon } from 'next-share'
+
 
 interface Props {
   recipe: RecipeType,
@@ -20,75 +22,112 @@ const tabStyle = {
   width: '50%'
 }
 
-function RecipePage({recipe, errorCode}: Props): ReactElement {
-  
-  if(errorCode){
-    return <Error statusCode={errorCode}/>
-  }
-  
-    return (
-        <Layout title={recipe.name}>
-            <div className={styles.container}>
-                <div className={styles.intro}>
-                   <div className={styles.img}><Image layout='fill' src={recipe.image.images[0].url} alt="" /></div>
-                   <div className={styles.icons}>
-                       <div className={styles.iconItem}>
-                         <div className={styles.iconWrapper}><Image src='/stopwatch.png' alt='' layout='fill'/></div>
-                         <div>{recipe.time}</div>
-                       </div>
-                       <div className={styles.iconItem}>
-                         <div className={styles.iconWrapper}><Image src='/group.png' alt='' layout='fill'/></div>
-                         <div>{recipe.quantity}</div>
-                       </div>
-                       <div className={styles.iconItem}>
-                         <div className={styles.iconWrapper}><Image src='/dinner.png' alt='' layout='fill'/></div>
-                         <div>{recipe.category.name}</div>
-                       </div>
-                   </div>
-                </div>
-                <User user={recipe.author}/>
-                <div>
-                  <p>{recipe.description}</p>
-                </div>
-          <div className={styles.tabs}>
-            <Tabs>
-              <TabList>
-                <Tab style={tabStyle}><div className={styles.tab}><b>Ingredients</b></div></Tab>
-                <Tab style={tabStyle}><div className={styles.tab}><b>Instructions</b></div></Tab>
-              </TabList>
-              <TabPanel>
-                <div>
-                  <ul>
-                    {
-                      recipe.ingredients.map(ingr => (
-                        <li key={ingr._id}>{ingr.item}</li>
-                      ))
-                    }
-                  </ul>
-                </div>
-              </TabPanel>
-              <TabPanel>
-                <div>
-                  <ol>
-                    {
-                      recipe.instructions.map(inst=> (
-                        <li key={inst._id}>{inst.item}</li>
-                      ))
-                    }
-                  </ol>
-                </div>
-              </TabPanel>
-            </Tabs>
-          </div>
 
+
+function RecipePage({ recipe, errorCode }: Props): ReactElement {
+
+  const [fullUrl, setFullUrl] = useState('')
+
+  useEffect(()=> {
+     setFullUrl(`${window.location.protocol}//${window.location.host}${window.location.pathname}`)
+  }, [fullUrl])
+
+  if (errorCode) {
+    return <Error statusCode={errorCode} />
+  }
+
+  return (
+    <Layout title={recipe.name} 
+    recipeData={{
+      title: recipe.name, 
+      description: recipe.description.substring(0, 100),
+      url: fullUrl,
+      image: recipe.image.images[0].url
+    }}>
+      <div className={styles.container}>
+        <div className={styles.intro}>
+          <div className={styles.img}><Image layout='fill' src={recipe.image.images[0].url} alt="" /></div>
+          <div className={styles.icons}>
+            <div className={styles.iconItem}>
+              <div className={styles.iconWrapper}><Image src='/stopwatch.png' alt='' layout='fill' /></div>
+              <div>{recipe.time}</div>
             </div>
-        </Layout>
-    )
+            <div className={styles.iconItem}>
+              <div className={styles.iconWrapper}><Image src='/group.png' alt='' layout='fill' /></div>
+              <div>{recipe.quantity}</div>
+            </div>
+            <div className={styles.iconItem}>
+              <div className={styles.iconWrapper}><Image src='/dinner.png' alt='' layout='fill' /></div>
+              <div>{recipe.category.name}</div>
+            </div>
+          </div>
+        </div>
+        <User user={recipe.author} />
+        <div>
+          <p>{recipe.description}</p>
+        </div>
+        <div className={styles.tabs}>
+          <Tabs>
+            <TabList>
+              <Tab style={tabStyle}><div className={styles.tab}><b>Ingredients</b></div></Tab>
+              <Tab style={tabStyle}><div className={styles.tab}><b>Instructions</b></div></Tab>
+            </TabList>
+            <TabPanel>
+              <div>
+                <ul>
+                  {
+                    recipe.ingredients.map(ingr => (
+                      <li key={ingr._id}>{ingr.item}</li>
+                    ))
+                  }
+                </ul>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div>
+                <ol>
+                  {
+                    recipe.instructions.map(inst => (
+                      <li key={inst._id}>{inst.item}</li>
+                    ))
+                  }
+                </ol>
+              </div>
+            </TabPanel>
+          </Tabs>
+        </div>
+        </div>
+        <div className={styles.shareButtons}>
+          <FacebookShareButton
+            url={fullUrl}
+            title={recipe.name}
+            hashtag={'#deliciouse'}
+          >
+            <FacebookIcon size={50} round />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={fullUrl}
+            title={recipe.name}
+            hashtags={['deliciouse']}
+          >
+            <TwitterIcon size={50} round />
+          </TwitterShareButton>
+
+          <WhatsappShareButton
+            url={fullUrl}
+            title={recipe.name}
+          >
+            <WhatsappIcon size={50} round />
+          </WhatsappShareButton>
+        </div>
+    </Layout>
+  )
 }
 
-export default  RecipePage
+export default RecipePage
 
-export async function getServerSideProps({params}: GetServerSidePropsContext){
+export async function getServerSideProps({ params }: GetServerSidePropsContext) {
   try {
     const slug = params?.slug
     const recipeRes = await axios.get(`${API_URL}/recipes/${slug}`)
